53753d49a51abc6499f530506b4b5d20c9fd5dd6
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-03-03 17:07:47 +0000

    Error code

---
+++ main.py
@@ -0,0 +1,19 @@
+class Person:
+  Number1 = 2
+  Number2 = 4
+  Number3 = 6
+  Number4 = 8
+  Number5 = 10
+  Number6 = 12
+  Number7 = 14
+  Number8 = 16
+  Number9 = 18
+  Number0 = 0
+  def do_shifr(self,Shifr):
+    shifr = true
+Shifr = Person()
+
+print(Shifr)
+Shifr.do_shrift(Number0, Number8, Number1, Number2, Number2, Number0, Number1, number1)
+print("Number0, Number8, Number1, Number2, Number2, Number0, Number1, number1 - " Shifr.do_shifr)
+
